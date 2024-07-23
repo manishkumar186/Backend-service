426c7c11b62eb1599e57a73bb6ab331ab5bddbef
Manish Kumar <maniskmr555@gmail.com>
2024-07-23 08:31:49 +0530

    cross origin apply

--- src/main/java/com/example/Medilife_backend/MedilifeBackendApplication.java
+++ src/main/java/com/example/Medilife_backend/MedilifeBackendApplication.java
@@ -2,12 +2,27 @@ package com.example.Medilife_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MedilifeBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedilifeBackendApplication.class, args);
+	}
+
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/medilife/**")
+					.allowedOrigins("*")
+					.allowedMethods("*")
+					.allowedHeaders("*");
+
+		}
 	}
 
 }
